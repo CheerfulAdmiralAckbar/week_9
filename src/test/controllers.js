f93337801b2596a83f3b83c8a1435091d02640c0
AdmiralAckbar411 <admiralackbar411@gmail.com>
2024-07-15 14:30:51 +0100

    Committing code

--- src/test/controllers.js
+++ src/test/controllers.js
@@ -1,9 +1,10 @@
 const postTest = async (req, res) => {
   console.log("postTest controller hit and username: ", req.body.username);
+  const username = req.body.username;
   try {
     // db code
 
-    res.status(201).json({ message: "success", body: req.body });
+    res.status(201).json({ message: "success", body: username.toLowerCase() });
   } catch (error) {
     res.status(500).json({ message: error.message, error: error });
   }
